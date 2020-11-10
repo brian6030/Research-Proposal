1bedb7190e63f11d35f230188ce33b82a99b397a
brian6030 <brian2009122@gmail.com>
2020-11-10 21:54:20 +0800

    Depth Occlusion

---
+++ Assets/Scripts/PlaceObjectsOnPlane.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.EventSystems;
+
+public class PlaceObjectsOnPlane : MonoBehaviour
+{
+    ARPlaneManager planeManager;
+    ARRaycastManager raycastManager;
+
+    List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    public GameObject placedPrefab;
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+        planeManager = GetComponent<ARPlaneManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            PlaceObject();
+        }
+    }
+
+    void PlaceObject()
+    {
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began && !IsPointerOverUIObject())
+        {
+            if (raycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = s_Hits[0].pose;
+                Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+            }
+        }
+    }
+
+    bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+
+        return results.Count > 0;
+    }
+}
